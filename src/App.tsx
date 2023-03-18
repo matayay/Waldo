43b931a0821b6d7242a4482768ceefe38e525d14
Matayay Karuna <matayay2@gmail.com>
2023-03-17 18:07:13 -0600

    added image

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import waldo_image from './assets/waldoimg.png'
 import './stylesheets/App.css'
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='picture'>
+        <img src={waldo_image} alt="Where's Waldo Picture." />
+      </div>
     </div>
   )
 }
