77b3e43add42a34dd2fa980acd1b4395f5a9b0e8
Matayay Karuna <matayay2@gmail.com>
2023-03-18 14:29:11 -0600

    finished most frontend stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
-import { useState } from 'react'
-import waldo_image from './assets/waldoimg.jpeg'
-import waldo from './assets/waldo.jpg'
-import odlaw from './assets/odlaw.jpg'
-import wizard from './assets/wizard.jpg'
-import './stylesheets/App.css'
+import { useState } from "react";
+import waldo_image from "./assets/waldoimg.jpeg";
+import waldo from "./assets/waldo.jpg";
+import odlaw from "./assets/odlaw.jpg";
+import wizard from "./assets/wizard.jpg";
+import "./stylesheets/App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <div className='picture'>
-        <h1>Time:</h1>
-        <img id='main-pic' src={waldo_image} alt="Where's Waldo Picture." />
-        <div className='icons'>
-          <img src={wizard} alt="Picture of the wizard." />
-          <img src={waldo} alt="Picture of Waldo." />
-          <img src={odlaw} alt="Picture of Odlaw" />
+    const [Display, setDisply] = useState("none");
+    const [X, setX] = useState(0);
+    const [Y, setY] = useState(0);
+
+    const selection = (event: any) => {
+        setX(event.clientX);
+        setY(event.clientY);
+
+        if (Display == "none")
+        {
+            setDisply("block");
+        }
+
+        else
+        {
+            setDisply("none");
+        }
+    }
+
+    const handleForm = (event: any) => {
+        setDisply("none");
+
+        event.preventDefault();
+    }
+
+    const styles = {
+        display: Display,
+        left: `${X}px`,
+        top: `${Y}px`
+    }
+
+    return (
+        <div className="App">
+            <div className="popup" style={styles}>
+                <form onSubmit={handleForm}>
+                    <input type="submit" value="Wizard" />
+                    <input type="submit" value="Waldo" />
+                    <input type="submit" value="Odlaw" />
+                </form>
+            </div>
+
+            <div className="picture" onClick={selection}>
+                <h1>Time:</h1>
+                <img id="main-pic" src={waldo_image} alt="Where's Waldo Picture." />
+                <div className="icons">
+                    <div className="image">
+                        <img src={wizard} alt="Picture of the wizard." />
+                        <h4>Wizard</h4>
+                    </div>
+                    <div className="image">
+                        <img src={waldo} alt="Picture of Waldo." />
+                        <h4>Waldo</h4>
+                    </div>
+                    <div className="image"> 
+                        <img src={odlaw} alt="Picture of Odlaw" />
+                        <h4>Odlaw</h4>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    );
 }
 
-export default App
+export default App;
