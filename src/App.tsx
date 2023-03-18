6c38b04fd76f5d8b043a1da8e5ff0942eae53b39
Matayay Karuna <matayay2@gmail.com>
2023-03-18 00:01:28 -0600

    finished page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
-import waldo_image from './assets/waldoimg.png'
+import waldo_image from './assets/waldoimg.jpeg'
+import waldo from './assets/waldo.jpg'
+import odlaw from './assets/odlaw.jpg'
+import wizard from './assets/wizard.jpg'
 import './stylesheets/App.css'
 
 function App() {
   return (
     <div className="App">
       <div className='picture'>
-        <img src={waldo_image} alt="Where's Waldo Picture." />
+        <h1>Time:</h1>
+        <img id='main-pic' src={waldo_image} alt="Where's Waldo Picture." />
+        <div className='icons'>
+          <img src={wizard} alt="Picture of the wizard." />
+          <img src={waldo} alt="Picture of Waldo." />
+          <img src={odlaw} alt="Picture of Odlaw" />
+        </div>
       </div>
     </div>
   )
